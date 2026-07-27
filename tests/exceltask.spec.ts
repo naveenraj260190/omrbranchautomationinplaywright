0ca2b0d47b01719f91f586e8aebd19330bdebb37
naveenraj260190 <naveenraj260190>
2026-07-27 11:24:09 +0530

    hello

--- tests/exceltask.spec.ts
+++ tests/exceltask.spec.ts
@@ -3,7 +3,8 @@ import path from "node:path";
 import XLSX from "xlsx"
 
 
-test("helloExcel",async()=>
+test
+("helloExcel",async()=>
 {
 
  const browser = await firefox.launch();
@@ -19,12 +20,37 @@ type ExcelColumn =
     userName : string,
     passWord : string,
     expectdResult : string,
+   [key: string]: any;
 
   };
 
-  const sheetname:string = "login";
+   // assigning sheet name
+     let sheetname:string = "login";
+    //Mention the workbook -> instructing to read excel file
+    let workbook: XLSX.WorkBook = XLSX.readFile(excelPath);
+    //	- Get the required sheet using the sheet name
+    let sheet: XLSX.WorkSheet = workbook.Sheets[sheetname];
+    //	- Convert the excel sheet data into JSON
+    let jsonData: ExcelColumn[] = XLSX.utils.sheet_to_json<ExcelColumn>(sheet);
+    console.log("JSON data ", jsonData);
 
 
+      let res: ExcelColumn | undefined = jsonData.find(
+    (value) =>
+      value.testScenario === "Login with Valid Credentails",
+  );
 
+     if (res) 
+        {
+          //create a new column and set value
+     res.status = "updated";
+      }
+    
+      let updatedValue = XLSX.utils.json_to_sheet(jsonData);
+      workbook.Sheets[sheetname] = updatedValue;
+      XLSX.writeFile(workbook,excelPath);
+    
+      console.log("Done")
+    
 }
 )
